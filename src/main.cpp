1db5eaac4322a1431a98995988e4aecb457efdad
byjtew93 <benjamin.lozes@protonmail.com>
2021-12-11 13:31:16 +0100

    Adding the DEBUG_MODE from CMake BUILD_MODE

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,12 @@
 #include "TimeDiscretization.hpp"
 #include "Variable.hpp"
 
+// Defined by CMake [ DEFAULT: 1 ]
+// Usage for release: cmake <target> -DCMAKE_BUILD_TYPE=Release
+#if !defined(DEBUG_MODE)
+#define DEBUG_MODE 1
+#endif  // DEBUG_MODE
+
 int main(int argc, char const *argv[]) {
-    
     return 0;
 }
