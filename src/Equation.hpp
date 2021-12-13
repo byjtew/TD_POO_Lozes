4a87c7b9cb2ee51e77e42c3fc72506a73eb6957e
byjtew93 <benjamin.lozes@protonmail.com>
2021-12-13 23:15:00 +0100

    Templating Equation::compute with an Integration

--- src/Equation.hpp
+++ src/Equation.hpp
@@ -6,6 +6,19 @@
 
 namespace equation {
 
+namespace integrations {
+
+class EulerIntegration {
+   public:
+    static void update(variable::Variable &_variables, double _current_t,
+                       double _dt, std::function<double(double)> _f) {
+        double prev_t = _current_t - _dt;
+        _variables(_current_t) = _f(_current_t - prev_t) + _dt * prev_t;
+    }
+};
+
+}  // namespace integrations
+
 class Equation {
    private:
     std::function<double(double)> f;
@@ -13,8 +26,15 @@ class Equation {
    public:
     Equation(std::function<double(double)> func) : f(func){};
     void compute_initial_condition(double _t_initial,
-                                   variable::Variable &_variables);
-    void compute(double current_t, variable::Variable &_variables);
+                                   variable::Variable &_variables) {
+        _variables(_t_initial) = f(_t_initial);
+    }
+
+    template <class Integrator = integrations::EulerIntegration>
+    void compute(double current_t, variable::Variable &_variables) {
+        double dt = _variables.getTime().get_step();
+        Integrator::update(_variables, current_t, dt, f);
+    }
 
     std::function<double(double)> getFunction() { return f; }
 };
