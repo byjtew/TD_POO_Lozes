85c74b5e43c13bf3adad74edbfd755bc241f756c
byjtew93 <benjamin.lozes@protonmail.com>
2021-12-16 18:04:28 +0100

    Stable test on main

--- src/main.cpp
+++ src/main.cpp
@@ -11,29 +11,38 @@
 #include "Timer.hpp"
 #include "Variable.hpp"
 
+template <class Integrator_A, class Integrator_B>
+void testSolvingParallel(time_discretization::ITimeDiscretization *time,
+                         std::function<double(double)> f, double exact_result) {
+    equation::Equation eq(f);
+    problem::Problem problem_A(eq, time), problem_B(eq, time);
+    std::pair<timer::Timer, timer::Timer> timers(timer::Timer{}, timer::Timer{});
+
+    auto results = problem::Problem::solve_parallel<Integrator_A, Integrator_B>(
+        problem_A, problem_B, timers);
+
+    std::cout
+        << "-- Solving problem_A result comapred to the correct result is: |"
+        << exact_result << " - " << results.first.getLast()
+        << "| = " << std::fabs(exact_result - results.first.getLast()) << " --"
+        << std::endl;
+    std::cout << "-- Solving of problem_A took "
+              << timers.first.printInSeconds() << " seconds --" << std::endl;
+
+    std::cout
+        << "-- Solving problem_B result comapred to the correct result is: |"
+        << exact_result << " - " << results.second.getLast()
+        << "| = " << std::fabs(exact_result - results.second.getLast()) << " --"
+        << std::endl;
+    std::cout << "-- Solving of problem_B took "
+              << timers.second.printInSeconds() << " seconds --" << std::endl;
+}
+
 int main(int argc, char const *argv[]) {
-    time_discretization::UniformTimeDiscretization unif_time(0, 100, 1e6);
-    equation::Equation eq0([](double x) { return 1; });
-    problem::Problem problem0(eq0, &unif_time);
-    double true_result = 100;
-    variable::Variable results0(&unif_time);
-
-    timer::Timer timer0{};
-
-    std::thread first_thread([&problem0, &results0, &timer0]() {
-        timer0.start();
-        results0 = problem0.solve();
-        timer0.stop();
-    });
-
-    first_thread.join();
-    std::cout << "-- Solving took " << timer0.printInSeconds() << " seconds --"
-              << std::endl;
-
-    std::cout << "-- Solving difference with the correct result is: |"
-              << true_result << " - " << results0.getLast() << "| = "
-              << std::fabs(true_result - results0.getLast()) << " --"
-              << std::endl;
+    time_discretization::UniformTimeDiscretization unif_time(0, 10, 1e5);
+    testSolvingParallel<equation::integrations::EulerIntegration,
+                        equation::integrations::RungeKuttaIntegration>(
+        &unif_time, [](double x) { return x; }, 10);
 
     return 0;
 }
